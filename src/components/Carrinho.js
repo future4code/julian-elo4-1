4a9946bf1d03fa090a4c7620b1452247b3cf5813
Wendel de Lima <wendel.delima94@gmail.com>
2020-05-07 13:47:56 -0300

    Botao do carrinho

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import styled from 'styled-components'
 
-import Botao from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button'
 import ShopIcon from '@material-ui/icons/AddShoppingCart'
 
 const Pontas = styled.div`
     height: 20vh;
     background-color: #ff6f00;
     color: #fff8e1;
+`
+const BotaoAdicionar = styled.div`
+   display: flex;
+   align-items: flex-end;
+   justify-content: flex-start;
+
+`
+const BotaoFinalizar = styled.div`
+    
+
+
+  
+    
+
 `
 
 
@@ -19,14 +33,12 @@ export class Carrinho extends React.Component{
     
       <Pontas><h1>Header</h1></Pontas>
       <br/>
-<Botao variant="fab" color="primary" size="medium" ><ShopIcon /></Botao>
-
-<br/>
-<br/>
-<Botao variant="contained" color="primary" size="small" >Finalizar compra</Botao>
-      
- 
-  
+      <BotaoAdicionar>
+<Button variant="fab" color="primary" size="medium" ><ShopIcon /></Button>
+      </BotaoAdicionar> 
+      <BotaoFinalizar> 
+<Button variant="contained" color="primary" size="small" >Finalizar compra</Button>
+      </BotaoFinalizar>
         <Pontas><h1>Rodapé</h1></Pontas>
       </div>
 
